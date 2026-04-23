b5e93d9d4f6f9289a05f4f07c5398ead72e7a510
Konrad Baran <konradbaran95@gmail.com>
2026-04-23 21:06:34 +0200

    feat (pages): add modifications in LoginPage class

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,15 +1,37 @@
 """missing docstring"""
 
-from playwright.sync_api import Page
-from utils.config import Config
 from pages.base_page import BasePage
+from utils.config import Config
+from playwright.sync_api import Page
 
 
 class LoginPage(BasePage):
 
     def __init__(self, page: Page):
         super().__init__(page)
+        self.username_input = page.locator("[data-test=\"username\"]")
+        self.password_input = page.locator("[data-test=\"password\"]")
+        self.login_button = page.locator("[data-test=\"login-button\"]")
+        self.error_message = page.locator("[data-test=\"error\"]")
+        self.error_message_close_button = page.locator("[data-test=\"error-button\"]")
+
 
     def navigate(self):
         """Opens login page in the browser"""
         self.page.goto(Config.BASE_URL)
+
+
+    def login(self, username, password):
+        """Execute login with given username and password"""
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
+
+
+    def get_error_text(self):
+        """Returns text of error message displayed after failed login"""
+        return self.error_message.text_content()
+
+
+    def close_error(self):
+        """Closes error message box"""
