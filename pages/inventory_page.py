a16cc6fec45571f714e75167818bf873e80ca8f7
Konrad Baran <konradbaran95@gmail.com>
2026-04-27 21:35:30 +0200

    feat (pages): add aditional methods to InventoryPage class

---
+++ pages/inventory_page.py
@@ -0,0 +1,58 @@
+"""missing docstring"""
+
+from pages.base_page import BasePage
+from utils.config import Config
+from playwright.sync_api import Page
+
+
+class InventoryPage(BasePage):
+
+    def __init__(self, page: Page):
+        super().__init__(page)
+        self.page_title = page.locator("[data-test=\"title\"]")
+        self.inventory_items = page.locator(".inventory_item")
+        self.cart_badge = page.locator(".shopping_cart_badge")  # co to jest? nie znalazłem takiego lokatora
+        self.cart_link = page.locator(".shopping_cart_link")  # dlaczego w ten sposób?
+        self.cart_button = page.locator("[data-test=\"shopping-cart-link\"]")  # a nie w ten sposób?
+        self.product_sort_dropdown = page.locator("[data-test=\"product-sort-container\"]")
+        self.item_names = page.locator(".inventory_item_name")
+        self.item_prices = page.locator(".inventory_item_price")
+        self.add_buttons = page.locator("button[data-test^=\"add-to-cart\"]")
+        self.remove_buttons = page.locator("button[data-test^=\"remove\"]")
+        self.burger_menu = page.locator("#react-burger-menu-btn")  # dlaczego tak? co to za lokator?
+        self.burger_menu_ver2 = page.get_by_role("button", name="Open Menu")  # dlaczego nie w ten sposób?
+        # dodane przez mnie:
+        self.footer_twitter_button = page.locator("[data-test=\"social-twitter\"]")
+        self.footer_fb_button = page.locator("[data-test=\"social-facebook\"]")
+        self.footer_linkedin_button = page.locator("[data-test=\"social-linkedin\"]")
+
+
+ # metody odczytujące stan strony:
+    def get_product_count(self) -> int:
+        return self.inventory_items.count()
+    
+    def all_product_names(self) -> list[str]:
+        return self.item_names.all_text_contents()
+    
+    def get_all_prices(self) -> list[float]:
+        texts = self.item_prices.all_text_contents()
+        return [float(price.replace("$", "")) for price in texts]
+
+    def get_cart_count(self) -> int:
+        if self.cart_badge.is_
+
+# metody wykonujące akcje
+    def add_product_by_index(self, index: int):
+        pass
+
+    def add_product_by_name(self):
+        pass
+
+    def sort_by(self):
+        pass
+
+    def go_to_cart(self):
+        pass
+
+    def open_menu(self):
+        pass
