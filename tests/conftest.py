3ceaecba400b560942017b8ee1de8fa2d2aaf268
Konrad Baran <konradbaran95@gmail.com>
2026-04-29 14:10:20 +0200

    feat (fixtures): implement configuration fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,27 @@
 """fixtures"""
 
-from pages.login_page import LoginPage
-from playwright.sync_api import Page
-
 import pytest
+import logging
+from playwright.sync_api import Page
+from pages.login_page import LoginPage
+from data.users import UserFactory
+from utils.config import Config
 
 
 @pytest.fixture
 def login_page(page: Page):
     """Returns an instance of LoginPage class"""
     return LoginPage(page)
+
+
+# Logger
+logger = logging.getLogger("swag_labs_tests")
+
+
+# --- CONFIGURATION ---
+@pytest.fixture(autouse=True)
+def setup_page(page: Page):
+    """Sets automaticaly timeouts for each test"""
+    page.set_default_timeout(Config.DEFAULT_TIMEOUT)
+    page.set_default_navigation_timeout(Config.NAVIGATION_TIMEOUT)
+    yield page
