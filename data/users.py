4814d8ebf2986bd1ae642d47b4e907e79198687a
Konrad Baran <konradbaran95@gmail.com>
2026-05-04 08:13:13 +0200

    feat (users): implement UserFactory class

--- data/users.py
+++ data/users.py
@@ -13,5 +13,48 @@ class UserFactory:
 
     @staticmethod
     def standard_user() -> User:
-        return User("standard_user", "secret_sauce", "the website is working properly")
+        return User(
+            "standard_user",
+            "secret_sauce",
+            "website works properly",
+        )
     
+    @staticmethod
+    def locked_out_user() -> User:
+        return User(
+            "locked_out_user",
+            "secret_sauce",
+            "blocked user, cannot log in",
+        )
+    
+    @staticmethod
+    def problem_user() -> User:
+        return User(
+            "problem_user",
+            "secret_sauce",
+            "website has several defects",
+        )
+    
+    @staticmethod
+    def performance_glitch_user() -> User:
+        return User(
+            "performance_glitch_user", 
+            "secret_sauce", 
+            "website simulates slow connection",
+        )
+    
+    @staticmethod
+    def error_user() -> User:
+        return User(
+            "error_user",
+            "secret_sauce",
+            "API returns code 500 errors",
+        )
+    
+    @staticmethod
+    def visual_user() -> User:
+        return User(
+            "visual_user",
+            "secret_sauce",
+            "website contains visual glitches",
+        )
