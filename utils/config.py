ef796a346c35560c6c7695db8eedd35f4565b118
Konrad Baran <konradbaran95@gmail.com>
2026-05-05 21:31:47 +0200

    feat (config): add social media profiles' url to config.py

--- utils/config.py
+++ utils/config.py
@@ -6,6 +6,10 @@ class Config:
     PRODUCTS_URL = os.getenv("PRODUCTS_URL", "https://www.saucedemo.com/inventory.html")
     CART_URL = os.getenv("CART_URL", "https://www.saucedemo.com/cart.html")
 
+    FB_URL = os.getenv("FB_URL", "https://www.facebook.com/saucelabs")
+    X_URL = os.getenv("X_URL", "https://x.com/saucelabs")
+    LINKEDIN_URL = os.getenv("LINKEDIN_URL", "https://www.linkedin.com/company/sauce-labs/")
+
     DEFAULT_TIMEOUT = int(os.getenv("TIMEOUT", "10000"))
     NAVIGATION_TIMEOUT = int(os.getenv("NAV_TIMEOUT", "30000"))
     SLOW_USER_TIMEOUT = 15000
